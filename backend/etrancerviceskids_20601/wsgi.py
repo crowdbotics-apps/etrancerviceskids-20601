eff62c56f13d2bf33fab99e857422ddbae98f495
Crowdbotics <team@crowdbotics.com>
2020-09-23 13:07:15 +0000

    Update models

--- backend/etrancerviceskids_20601/wsgi.py
+++ backend/etrancerviceskids_20601/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'etrancerviceskids_20601.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etrancerviceskids_20601.settings")
 
 application = get_wsgi_application()
